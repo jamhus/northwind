a2177b5160a545d93d1ba12808900847d2c96a69
Jamal Hussein <jamal.hussein@brilliantfuture.se>
2025-11-05 14:57:00 +0100

    remove footer

--- Northwind.Client/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center text-sm text-gray-500 py-4 border-t bg-gray-50 mt-8">
-      <p>© {new Date().getFullYear()} Northwind Demo</p>
-    </footer>
-  );
-}
--- Northwind.Client/src/routes/AppRoutes.tsx
+++ Northwind.Client/src/routes/AppRoutes.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/products/ProductsPage";
 import Navbar from "../components/layout/Navbar";
-import Footer from "../components/layout/Footer";
 
 export default function AppRoutes() {
   return (
@@ -15,7 +14,6 @@ export default function AppRoutes() {
             <Route path="/products" element={<ProductsPage />} />
           </Routes>
         </div>
-        <Footer />
       </div>
     </BrowserRouter>
   );
